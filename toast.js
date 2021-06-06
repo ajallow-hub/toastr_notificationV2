1cf6222bdcba2034b50e610fcf3f995c9b41e468
alie <53941410+ajallow-hub@users.noreply.github.com>
2021-06-06 15:14:49 +0000

    toast.js

---
+++ toast.js
@@ -0,0 +1,22 @@
+class Toastr {
+    // options = {};
+  
+    static showNotification(position, text = "I am a notification") {
+      const notification = document.createElement("div");
+      notification.textContent = text;
+      notification.classList.add("toastr", position);
+      document.querySelector("body").style.position = "relative";
+      document.querySelector("body").append(notification);
+      this.removeNotification("div");
+    }
+  
+    static removeNotification(element) {
+      setTimeout(() => {
+        document.querySelector("body").style.position = "static";
+        document.querySelector(element).style.setProperty("opacity", 0);
+        setTimeout(() => {
+          document.querySelector(element).remove();
+        }, 1000);
+      }, 4000);
+    }
+  }
